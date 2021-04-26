cdac42c53626b88c1059f66f4cdc6754ab480a1e
CrispyyBaconx <immabaconback@gmail.com>
2021-04-26 04:03:25 -0400

    Add project files.

---
+++ AutoMeeter/Joiner.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace AutoMeeter
+{
+    partial class Joiner
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Joiner));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SystemTimeLabel = new System.Windows.Forms.Label();
+            this.SystemTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(30, 573);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(124, 35);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(337, 643);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(179, 573);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(124, 35);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(54)))));
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(54, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 50);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Meeting IDs";
+            // 
+            // SystemTimeLabel
+            // 
+            this.SystemTimeLabel.AutoSize = true;
+            this.SystemTimeLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.SystemTimeLabel.Font = new System.Drawing.Font("Unispace", 71.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.SystemTimeLabel.ForeColor = System.Drawing.Color.White;
+            this.SystemTimeLabel.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.SystemTimeLabel.Location = new System.Drawing.Point(409, 21);
+            this.SystemTimeLabel.Name = "SystemTimeLabel";
+            this.SystemTimeLabel.Size = new System.Drawing.Size(285, 115);
+            this.SystemTimeLabel.TabIndex = 4;
+            this.SystemTimeLabel.Text = "Null";
+            // 
+            // SystemTimer
+            // 
+            this.SystemTimer.Interval = 1000;
+            this.SystemTimer.Tick += new System.EventHandler(this.SystemTimer_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.pictureBox2.Location = new System.Drawing.Point(333, -2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(777, 196);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Joiner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1104, 641);
+            this.Controls.Add(this.SystemTimeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1120, 680);
+            this.MinimumSize = new System.Drawing.Size(1120, 680);
+            this.Name = "Joiner";
+            this.Text = "Auto Meeting Joiner";
+            this.Load += new System.EventHandler(this.Joiner_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label SystemTimeLabel;
+        private System.Windows.Forms.Timer SystemTimer;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ AutoMeeter/Joiner.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AutoMeeter
+{
+    public partial class Joiner : Form
+    {
+        public Joiner()
+        {
+            InitializeComponent();
+        }
+
+        private void Joiner_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.Sizable;
+            MaximizeBox = false; // ensure
+            StopButton.Enabled = false;
+            SystemTimer.Enabled = true;
+        }
+
+        private static string GetDefaultBrowserPath()
+        {
+            string key = @"htmlfile\shell\open\command";
+            RegistryKey registryKey = Registry.ClassesRoot.OpenSubKey(key, false);
+            return ((string)registryKey.GetValue(null, null)).Split('"')[1];
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            StopButton.Enabled = true;
+            StartButton.Enabled = false;
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            StartButton.Enabled = true;
+            StopButton.Enabled = false;
+        }
+
+        private void SystemTimer_Tick(object sender, EventArgs e)
+        {
+            SystemTimeLabel.Text = DateTime.Now.ToString()[DateTime.Now.ToString().IndexOf(" ")..];
+        }
+    }
+}
